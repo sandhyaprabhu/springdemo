5ba9e9118f6a0849d4de90f4b318f9c846bc7490
sandhya.prabhu <san.n3005@gmail.com>
2024-11-27 09:36:33 +0000

    added gitignore and few more changes

--- src/main/java/com/gl/springBootDemo/GLController.java
+++ src/main/java/com/gl/springBootDemo/GLController.java
@@ -6,20 +6,29 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+
 @RestController
 public class GLController {
 
-    @RequestMapping(value="/v0/message", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    HashMap<String, Object> response = new HashMap<>();
+
+    @RequestMapping(value="/v0/getMessage", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Object getMessage1(){
        return  ResponseEntity.status(HttpStatus.ACCEPTED).body(new ApiResponse("Successful Message 1"));
     }
-    @RequestMapping(value="/v1/message", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value="/v0/sendMessage", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public Object postMessage1(
-            @RequestParam (required = true, defaultValue = "Smith") String firstName,
-            @RequestParam (required = false) String lastName){
-        if(firstName.isEmpty()){
-            return  ResponseEntity.status(HttpStatus.ACCEPTED).body(new ApiResponse("First Name Required"));
+            @RequestParam (required = true) String firstName,
+            @RequestParam (required = false) String middleName,
+            @RequestParam (required = true) String lastName){
+        if(firstName.isEmpty() || lastName.isEmpty()){
+            response.put("Error","First Name and Last Name is mandatory");
+            return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+           // return  ResponseEntity.status(HttpStatus.ACCEPTED).body(new ApiResponse("First Name Required"));
         }
-        return  ResponseEntity.status(HttpStatus.ACCEPTED).body(new ApiResponse("Successful Post Message1"+ " for " +firstName + " " +lastName) );
+        response.put("Success","Send Message"+ " for " +firstName + " " +lastName);
+        return  ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
+      //  return  ResponseEntity.status(HttpStatus.ACCEPTED).body(new ApiResponse("Successful Post Message1"+ " for " +firstName + " " +lastName) );
     }
 }
